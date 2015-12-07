d27e28b3ed3da3e0f8f89cc5b4bcbe14f44d8678
Maria José Barrile Tomazella <Maria Jos Barrile Tomazella>
2015-12-07 17:56:56 -0200

    Starting commit

---
+++ project/Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script holds all the player data, like level, experience, money, etc.
+/// </summary>
+public class PlayerData
+{
+
+}
---
+++ project/Assets/Scripts/Recording/ProductionManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// This script manage the production mechanic: the production on the scenery, new productions, what happens when a production is over.
+/// </summary>
+public class ManageProduction : MonoBehaviour
+{
+
+}
