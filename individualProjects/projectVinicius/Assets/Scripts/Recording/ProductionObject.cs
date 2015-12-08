9f0cece4a0b5f6e869c1a7c4d5e075daeff49202
Vinicius Carvalho <Vinicius Carvalho>
2015-12-08 10:39:01 -0200

    Projeto ilidio adcionado

---
+++ individualProjects/projectVinicius/Assets/Scripts/Recording/ProductionObject.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script controls the behavior of the a production object.
+/// </summary>
+public class ProductionObject : MonoBehaviour
+{
+	
+}
+
