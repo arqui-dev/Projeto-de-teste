5890a9acb92f14d5dacf20fb7d562b127a3acc02
Vinicius Carvalho <Vinicius Carvalho>
2015-12-10 14:24:06 -0200

    Utils

---
+++ individualProjects/projectVinicius/Assets/Scripts/Player/Utils.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utils : MonoBehaviour {
+
+	public int Hype (VideoData newVid, VideoData newOld) {
+		int sum = newVid 
+	}
+
+}
---
+++ individualProjects/projectVinicius/Assets/Scripts/Player/VideoData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoData : MonoBehaviour {
+		
+	public int SumAttributes () {
+		return PlayerData.skillInnovation.Level + PlayerData.skillQuality.Level +
+			PlayerData.skillContent.Level + PlayerData.skillCommunication.Level;
+	}
+
+	public int Hype () {
+ 		return PlayerData
+	}
+}
