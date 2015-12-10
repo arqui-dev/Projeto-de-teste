f6b8c7fe56e1c9acfa2e3482d62c5604fe98873f
Vinicius Carvalho <Vinicius Carvalho>
2015-12-10 18:29:28 -0200

    Script calculo video adcionado

--- individualProjects/projectVinicius/Assets/Scripts/Player/PlayerData.cs
+++ individualProjects/projectVinicius/Assets/Scripts/Player/PlayerData.cs
@@ -12,7 +12,7 @@ public class PlayerData
 	public enum SkillType {
 		Innovation, Quality, Content, Communication
 	}
-
+	
 	/// <summary>
 	/// Total skill types.
 	/// </summary>
@@ -24,30 +24,33 @@ public class PlayerData
 	public enum KnowledgeType {
 		Innovation, Quality, Content
 	}
-
+	
 	/// <summary>
 	/// Total skill types.
 	/// </summary>
 	public const int totalKnowledgeTypes = 18;
-
+	
 	/// <summary>
 	/// Player skills, quality, innovation and content.
 	/// </summary>
 	static public Skill [] skills;
-
+	
 	static public int [] skillBonus = new int[totalSkillTypes];
-
+	
 	static public Skill skillInnovation = new Skill("Innovation","Innovation");
 	static public Skill skillQuality = new Skill("Quality","Quality");
 	static public Skill skillContent = new Skill("Content","Content");
 	static public Skill skillCommunication = new Skill("Communication","Communication");
-
+	
 	/// <summary>
 	/// The SEBRAE are of knowledges
 	/// </summary>
 	static public Knowledge [] knowledges = 
 		new Knowledge[totalKnowledgeTypes];
-
+	
+	static public int scoreLastVideo = 0;
+	static public int scoreVideoBefore = 0;
+	
 	/// <summary>
 	/// The production level.
 	/// </summary>
@@ -61,23 +64,23 @@ public class PlayerData
 		}
 		return sum;
 	}
-
-
+	
+	
 	static public void Create()
 	{
 		skills = new Skill[totalSkillTypes];
-
+		
 		skills[0] = skillInnovation;
 		skills[1] = skillQuality;
 		skills[2] = skillContent;
 		skills[3] = skillCommunication;
-
+		
 		for(int i = 0; i < skillBonus.Length; i++)
 		{
 			skillBonus[i] = 0;
 		}
 	}
-
+	
 	static public int CalculateVideoScore(int [] levels)
 	{
 		int sum = 0;
@@ -85,10 +88,13 @@ public class PlayerData
 		{
 			sum += levels[i] + skillBonus[i];
 		}
-
+		
+		scoreVideoBefore = scoreLastVideo;
+		scoreLastVideo = sum;
+		
 		return sum;
 	}
-
+	
 	static public void EarnXP(int [] levels)
 	{
 		for (int i = 0; i < totalSkillTypes; i++)
@@ -105,16 +111,16 @@ public class Skill
 {
 	/// <summary> The skill title. </summary>
 	string title = "";
-
+	
 	/// <summary> Skill description. </summary>
 	string description = "";
-
+	
 	/// <summary> Skill level. </summary>
 	int level = 1;
-
+	
 	/// <summary> Skill total experience. </summary>
 	int xp = 0;
-
+	
 	public Skill(string title, string description)
 	{
 		this.title = title;
@@ -122,7 +128,7 @@ public class Skill
 		this.level = 1;
 		this.xp = 0;
 	}
-
+	
 	/// <summary>
 	/// Verifies if the skill is on the next level and raise it if it does.
 	/// </summary>
@@ -133,7 +139,7 @@ public class Skill
 			level++;
 		}
 	}
-
+	
 	/// <summary>
 	/// Necessary experience for the next level.
 	/// </summary>
@@ -142,10 +148,10 @@ public class Skill
 	{
 		return (level * (level + 1) / 2) * 10;
 	}
-
+	
 	//###############################################################
 	// Public methods
-
+	
 	/// <summary>
 	/// Earn experience and verify if the skill leveled up.
 	/// </summary>
@@ -155,21 +161,21 @@ public class Skill
 		this.xp += xp;
 		VerifyLevelUp();
 	}
-
+	
 	/// <summary> The level of the skill. </summary>
 	/// <returns>Level of the skill.</returns>
 	public int Level()
 	{
 		return level;
 	}
-
+	
 	/// <summary> The skill title. </summary>
 	/// <returns>Skill title.</returns>
 	public string Title()
 	{
 		return title;
 	}
-
+	
 	/// <summary> The skill description. </summary>
 	/// <returns>Skill description.</returns>
 	public string Description()
@@ -183,26 +189,26 @@ public class Skill
 /// </summary>
 public class Knowledge
 {
-
+	
 	string title = "";
-
+	
 	string description = "";
-
+	
 	bool learned = false;
-
+	
 	public string Title()
 	{
 		return title;
 	}
-
+	
 	public string Description()
 	{
 		return description;
 	}
-
+	
 	public bool Learned()
 	{
 		return learned;
 	}
-
+	
 }
